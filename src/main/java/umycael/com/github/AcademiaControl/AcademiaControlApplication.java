6e9ba7b02e7ce8f6c9234c1ec420e80537ecba27
umycael <mycael7ano@gmail.com>
2026-07-22 16:44:38 -0300

    first commit

---
+++ src/main/java/umycael/com/github/AcademiaControl/AcademiaControlApplication.java
@@ -0,0 +1,13 @@
+package umycael.com.github.AcademiaControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umycael/com/github/AcademiaControl/AcademiaControlApplicationTests.java
@@ -0,0 +1,13 @@
+package umycael.com.github.AcademiaControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
