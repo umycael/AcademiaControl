657a2f7a7691408c75bd8a1d097e8904e2642c36
umycael <mycael7ano@gmail.com>
2026-07-22 18:27:16 -0300

    feat: Cria o Repository e o Mapper da classe alunos.

---
+++ src/main/java/umycael/com/github/AcademiaControl/Aluno/AlunoMapper.java
@@ -0,0 +1,40 @@
+package umycael.com.github.AcademiaControl.Aluno;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AlunoMapper {
+
+        public AlunoModel map(AlunoDTO alunoDTO) {
+
+            AlunoModel alunoModel = new AlunoModel();
+            alunoModel.setId(alunoDTO.getId());
+            alunoModel.setNomeCompleto(alunoDTO.getNomeCompleto());
+            alunoModel.setCpf(alunoDTO.getCpf());
+            alunoModel.setEmail(alunoDTO.getEmail());
+            alunoModel.setTelefone(alunoDTO.getTelefone());
+            alunoModel.setDataNascimento(alunoDTO.getDataNascimento());
+            alunoModel.setSexo(alunoDTO.getSexo());
+            alunoModel.setAtivo(alunoDTO.isAtivo());
+            alunoModel.setDataCadastro(alunoDTO.getDataCadastro());
+
+            return alunoModel;
+        }
+
+        public AlunoDTO map(AlunoModel alunoModel) {
+            AlunoDTO alunoDTO = new AlunoDTO();
+            alunoDTO.setId(alunoModel.getId());
+            alunoDTO.setNomeCompleto(alunoModel.getNomeCompleto());
+            alunoDTO.setCpf(alunoModel.getCpf());
+            alunoDTO.setEmail(alunoModel.getEmail());
+            alunoDTO.setTelefone(alunoModel.getTelefone());
+            alunoDTO.setDataNascimento(alunoModel.getDataNascimento());
+            alunoDTO.setSexo(alunoModel.getSexo());
+            alunoDTO.setAtivo(alunoModel.isAtivo());
+            alunoDTO.setDataCadastro(alunoModel.getDataCadastro());
+
+            return alunoDTO;
+        }
+
+}
---
+++ src/main/java/umycael/com/github/AcademiaControl/Aluno/AlunoRepository.java
@@ -0,0 +1,6 @@
+package umycael.com.github.AcademiaControl.Aluno;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<AlunoModel, Long> {
+}
